4ac8d887493812408447bb4d8b562b32bec11bb8
IgorPelevanyuk <gavelock@gmail.com>
2014-12-16 06:58:07 +0300

    SAMLauncherAgent revised and combined with SAMCheckerAgent

--- DIRAC/FrameworkSystem/Agent/SAMLauncherAgent.py
+++ DIRAC/FrameworkSystem/Agent/SAMLauncherAgent.py
@@ -14,83 +14,93 @@ from DIRAC.FrameworkSystem.DB.SAMDB                         import SAMDB
 import os
 class SAMLauncherAgent( AgentModule ):
 
-  def initialize( self ):
-    return S_OK()
+    def _startNewTests(self):
+        # Get list of tests
+        testsToStart = []
+        dao = SAMDB()
+        result = dao.getTestsToStart()
+        if not result['OK']:
+            gLogger.error('Failed to load test to start')
+        else:
+            testsToStart = result['Value']
 
-  def execute( self ):
-    """ Main execution method
-    """
-    self.log.info( "    TICK!!" )
-    self.log.info( self.am_getOption("PollingTime") )
-    # Get list of tests
-    dao = SAMDB()
-    testList = dao.getTestsToRun()['Value']
-    print testList
-    # Leave only old and Finished
-    testsToRun = []
-    for test in testList:
-      self.log.info(str(test[3])+"  "+str(test[4]))
-      self.log.info(str(getTimeNow())+"  "+str(long(test[3].strftime('%s'))))
-      self.log.info(getTimeNow()-long(test[3].strftime('%s')))
+        # Create new Result db entity
+        for test in testsToStart:
+            site_id = test[0]
+            test_id = test[1]
+            site_name = test[2]
+            test_name = test[3]
+            executable = test[4]
 
-      if isTimeExceed(test[3], test[4]):
-        self.log.info(str(test[3])+"  "+str(test[4]))
-        self.log.info(str(getTimeNow())+"  "+str(long(test[3].strftime('%s'))))
-        self.log.info(getTimeNow()-long(test[3].strftime('%s')))
-        testsToRun.append(test)
-    # Create new Result db entity
-    for test in testsToRun:
-      result_id = dao.createNewResult(test[6], test[7])
-      wms_id = sendJob(test[0], test[2], result_id['Value'])
-      print 'wms_id ',  wms_id
-      if wms_id != 0:
-        dao.createJob(wms_id,test[6], result_id['Value'])
-      else:
-        dao.setResult('Fail', result_id['Value'], 'Failed to submit the job')
-    # Try to send job
-      #IF failed to get job_id - mark FAIL
-      #NEXT
-    # Create Job db entity
-    # Update result
-    return S_OK()
+            result = dao.startNewTest(site_id, test_id)
+            if not result['OK']:
+                break
+            result_id = result['Value']
+            executable = executable.split('&')
+            gLogger.info("SEND Job: "+site_name+"  "+executable[0])
+            dirac = Dirac()
+            j = Job()
+            j.setExecutable('python' , arguments=executable[0]+" "+result_id)
+            j.setInputSandbox(executable)
+            j.setName(test_name)
+            j.setDestination( site_name )
+            result = dirac.submit(j)
+            if result['OK']:
+                dao.addJobIdToResult(result_id, result['Value'])
+            else:
+                gLogger.error("Failed to submit the job: " + executable[0] + "to site "+site_name)
+                dao.setResult('Fail', result_id, 'Failed to submit the job:'+str(result))
+        return S_OK()
+
+    def _stopOldTests(self):
+        # Get list of tests
+        testsToStop = []
+        dao = SAMDB()
+        result = dao.getTestsToStop()
+        if not result['OK']:
+            gLogger.error('Failed to load test to stop')
+        else:
+            testsToStop = result['Value']
+
+        # Create new Result db entity
+        for test in testsToStop:
+            result_id = test[0]
+            wms_job_id = test[1]
+            timeout = test[2]
+
+            result = dao.setResult('Fail', result_id, 'Timeout fail after '+str(timeout/60)+' min of silence')
+            if not result['OK']:
+                gLogger.error('Failed to set result for result_id %s' % result_id)
+                break
+            
+            dirac = Dirac()
+            result = dirac.delete(int(wms_job_id))
+
+            if result['OK']:
+                gLogger.info('Successfully killed the job with id: %s' % wms_job_id)
+            else:
+                gLogger.error("Failed to kill the job with id: %s" % wms_job_id )
+        return S_OK()
+
+    def initialize( self ):
+        return S_OK()
+
+    def execute( self ):
+        self._startNewTests()
+        self._stopOldTests()
+        return S_OK()
   
-  def beginExecution(self):
-    self.log.info( "CYCLE START!!" )
+    def beginExecution(self):
+        self.log.info( "CYCLE START!!" )
     
-    return S_OK()
+        return S_OK()
 
-  def endExecution(self):
-    self.log.info( "CYCLE STOP!!" )
+    def endExecution(self):
+        self.log.info( "CYCLE STOP!!" )
 
-    return S_OK()
+        return S_OK()
 
-  def finalize(self):
-    self.log.info( "GRATEFUL EXIT. BYE" )
+    def finalize(self):
+        self.log.info( "GRATEFUL EXIT. BYE" )
     
-    return S_OK()
-
-import time
-def getTimeNow():
-  return long(time.time())
-
-def toMySQLTime(time_epoch):
-  stamp = time.gmtime(time_epoch)
-  return time.strftime('%Y-%m-%d %H:%M:%S', stamp)
-
-def toEpochTime(mysql_time):
-  stamp = time.strptime(mysql_time, "%Y-%m-%d %H:%M:%S")
-  return long(time.mktime(stamp))
-  
-def isTimeExceed(lastTime, timeout):
-  return getTimeNow()-long(lastTime.strftime('%s'))>timeout
-
-import urllib
-def sendJob(site, executable, result_id):
-  base = 'http://vm162.jinr.ru:8081/sendJob/'
-  url = base + site + '/' + executable + '/' + str(result_id)+'/'
-  try:
-    wms_id = urllib.urlopen(url).read()
-    print 'from rest ', wms_id
-  except:
-    wms_id = 0
-  return int(wms_id)
+        return S_OK()
