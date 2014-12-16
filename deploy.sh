0367dca01abfebfd4c5bab42b40f7cdd6f64e2d3
IgorPelevanyuk <gavelock@gmail.com>
2014-12-16 17:06:35 +0800

    Auxiliary files update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 cp DIRAC/FrameworkSystem/DB/SAMDB.py /opt/dirac/pro/DIRAC/FrameworkSystem/DB/.
 cp DIRAC/FrameworkSystem/DB/SAMDB.sql /opt/dirac/pro/DIRAC/FrameworkSystem/DB/.
 
+cp DIRAC/FrameworkSystem/Service/SAMHandler.py /opt/dirac/pro/DIRAC/FrameworkSystem/Service/SAMHandler.py
+cp DIRAC/FrameworkSystem/Client/SAMClient.py /opt/dirac/pro/DIRAC/FrameworkSystem/Client/SAMClient.py 
+
 cp DIRAC/FrameworkSystem/Agent/SAMLauncherAgent.py /opt/dirac/pro/DIRAC/FrameworkSystem/Agent/.
 
 cp sam_tests/wms_test.py /opt/dirac/pro/DIRAC/FrameworkSystem/Agent/sam_tests/.
--- load_back.sh
+++ load_back.sh
@@ -3,5 +3,8 @@ cp /opt/dirac/pro/DIRAC/FrameworkSystem/DB/SAMDB.sql DIRAC/FrameworkSystem/DB/SA
 
 cp /opt/dirac/pro/DIRAC/FrameworkSystem/Agent/sam_tests/wms_test.py sam_tests/wms_test.py
 
+cp /opt/dirac/pro/DIRAC/FrameworkSystem/Service/SAMHandler.py DIRAC/FrameworkSystem/Service/SAMHandler.py
+cp /opt/dirac/pro/DIRAC/FrameworkSystem/Client/SAMClient.py DIRAC/FrameworkSystem/Client/SAMClient.py
+
 cp /opt/dirac/pro/DIRAC/FrameworkSystem/Agent/SAMLauncherAgent.py DIRAC/FrameworkSystem/Agent/SAMLauncherAgent.py
 
