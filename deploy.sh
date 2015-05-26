a3d1c160f934ceefa57102c2c4d3e7188e3375f9
IgorPelevanyuk <gavelock@gmail.com>
2015-05-26 13:29:00 +0300

    Info about GeneralPurposeDB added to deploy and load_back scripts

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 cp DIRAC/FrameworkSystem/DB/SAMDB.py /opt/dirac/pro/DIRAC/FrameworkSystem/DB/.
 cp DIRAC/FrameworkSystem/DB/SAMDB.sql /opt/dirac/pro/DIRAC/FrameworkSystem/DB/.
 
+cp DIRAC/FrameworkSystem/DB/GeneralPurposeDB.py /opt/dirac/pro/DIRAC/FrameworkSystem/DB/.
+cp DIRAC/FrameworkSystem/DB/GeneralPurposeDB.sql /opt/dirac/pro/DIRAC/FrameworkSystem/DB/.
+
 cp DIRAC/FrameworkSystem/Service/SAMHandler.py /opt/dirac/pro/DIRAC/FrameworkSystem/Service/SAMHandler.py
 cp DIRAC/FrameworkSystem/Client/SAMClient.py /opt/dirac/pro/DIRAC/FrameworkSystem/Client/SAMClient.py 
 
--- load_back.sh
+++ load_back.sh
@@ -1,6 +1,9 @@
 cp /opt/dirac/pro/DIRAC/FrameworkSystem/DB/SAMDB.py DIRAC/FrameworkSystem/DB/SAMDB.py
 cp /opt/dirac/pro/DIRAC/FrameworkSystem/DB/SAMDB.sql DIRAC/FrameworkSystem/DB/SAMDB.sql
 
+cp /opt/dirac/pro/DIRAC/FrameworkSystem/DB/GeneralPurposeDB.py DIRAC/FrameworkSystem/DB/GeneralPurposeDB.py
+cp /opt/dirac/pro/DIRAC/FrameworkSystem/DB/GeneralPurposeDB.sql DIRAC/FrameworkSystem/DB/GeneralPurposeDB.sql
+
 cp /opt/dirac/pro/DIRAC/FrameworkSystem/Agent/sam_tests/* sam_tests/.
 
 cp /opt/dirac/pro/DIRAC/FrameworkSystem/Service/SAMHandler.py DIRAC/FrameworkSystem/Service/SAMHandler.py
